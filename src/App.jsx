0291ebe997010ad06d0c44dec93f11942ba0f041
Robert Hughes <rjh2869@gmail.com>
2026-09-17 12:53:08 -0500

    started app.jsx, showed welcome screen and connected playing state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import { useGame } from "./GameContext";
+import Welcome from "./Welcome";
+
 export default function App() {
-  return <></>;
+  const { playing } = useGame();
+
+  return (
+    <>
+      <h1>Whack-a-Mole</h1>
+      {playing ? <p>The game has begun!</p> : <Welcome />}
+    </>
+  );
 }
